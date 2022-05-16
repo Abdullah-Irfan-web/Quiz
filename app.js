a59ab6d6138b7ebc8afebb46c3064a9f9852cf5d
owaise irfan <abd.irfan7007@gmail.com>
2022-05-16 19:57:10 +0530

    quiz app hosted

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express=require('express');
 const app=express();
 const mongoose=require('mongoose');
 const path=require('path');
-
+const dotenv=require('dotenv');
+dotenv.config({path:'./config.env'})
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 app.use(express.static('public'));
-mongoose.connect('mongodb://localhost:27017/quiz',{
+const port=process.env.PORT || 3000;
+
+mongoose.connect(process.env.DATABASE,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
    
@@ -59,6 +62,6 @@ app.get('/',(req,res)=>{
     })
     
 })
-app.listen('3000',()=>{
+app.listen(port,()=>{
     console.log("listen to port 3000");
 });
